685cf7c9ed452ac21769cefe06216f1d7314f420
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2024-01-28 18:38:45 +0530

    feat: ticketing service

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from . import db
 # from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
 
 # class Note(db.Model):
@@ -15,3 +16,12 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True)
     oauth_id = db.Column(db.String(50), unique=True, nullable=False)
     # notes = db.relationship('Note')
+
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    oauth_id = db.Column(db.String(50), unique=True, nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
